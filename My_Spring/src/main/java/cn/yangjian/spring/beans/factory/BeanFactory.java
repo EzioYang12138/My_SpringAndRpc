082fb6d749fb1f273edffcf8cdae27b675c7d550
jdkYang <940604949@qq.com>
2020-06-26 19:40:27 +0800

    容器接口

---
+++ My_Spring/src/main/java/cn/yangjian/spring/beans/factory/BeanFactory.java
@@ -0,0 +1,18 @@
+package cn.yangjian.spring.beans.factory;
+
+import cn.yangjian.spring.beans.config.BeanDefinition;
+import cn.yangjian.spring.exception.BeansException;
+import cn.yangjian.spring.exception.NoSuchBeanDefinitionException;
+
+public interface BeanFactory {
+
+    Object getBean(String name) throws BeansException;
+
+    <T> T getBean(String name, Class<T> requiredType) throws BeansException;
+
+    boolean containsBeanDefintion(String beanDefinitionName);
+
+    boolean isSingleton(String beanDefinitionName) throws NoSuchBeanDefinitionException;
+
+    BeanDefinition getBeanDefinition(String beanDefinitionName);
+}
