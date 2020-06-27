18681e826a922bdfbc30f1cbcd5cc29317b178fd
jdkYang <940604949@qq.com>
2020-06-27 10:51:28 +0800

    对象代理

---
+++ My_Rpc/src/main/java/cn/yangjian/rpc/client/proxy/ObjectProxy.java
@@ -0,0 +1,130 @@
+package cn.yangjian.rpc.client.proxy;
+
+import cn.yangjian.rpc.client.ConnectManage;
+import cn.yangjian.rpc.client.RPCFuture;
+import cn.yangjian.rpc.client.RpcClientHandler;
+import cn.yangjian.rpc.protocol.RpcRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+
+public class ObjectProxy<T> implements InvocationHandler, IAsyncObjectProxy {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ObjectProxy.class);
+    private final Class<T> clazz;
+
+    public ObjectProxy(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (Object.class == method.getDeclaringClass()) {
+            String name = method.getName();
+            switch (name) {
+                case "equals":
+                    return proxy == args[0];
+                case "hashCode":
+                    return System.identityHashCode(proxy);
+                case "toString":
+                    return proxy.getClass().getName() + "@" +
+                            Integer.toHexString(System.identityHashCode(proxy)) +
+                            ", with InvocationHandler " + this;
+                default:
+                    throw new IllegalStateException(String.valueOf(method));
+            }
+        }
+
+        RpcRequest request = new RpcRequest();
+        request.setRequestId(UUID.randomUUID().toString());
+        request.setClassName(method.getDeclaringClass().getName());
+        request.setMethodName(method.getName());
+        request.setParameterTypes(method.getParameterTypes());
+        request.setParameters(args);
+        // Debug
+        LOGGER.debug(method.getDeclaringClass().getName());
+        LOGGER.debug(method.getName());
+        for (int i = 0; i < method.getParameterTypes().length; ++i) {
+            LOGGER.debug(method.getParameterTypes()[i].getName());
+        }
+        for (Object arg : args) {
+            LOGGER.debug(arg.toString());
+        }
+
+        RpcClientHandler handler = ConnectManage.getInstance().chooseHandler();
+        RPCFuture rpcFuture = handler.sendRequest(request);
+        return rpcFuture.get();
+    }
+
+    @Override
+    public RPCFuture call(String funcName, Object... args) {
+        RpcClientHandler handler = ConnectManage.getInstance().chooseHandler();
+        RpcRequest request = createRequest(this.clazz.getName(), funcName, args);
+        RPCFuture rpcFuture = handler.sendRequest(request);
+        return rpcFuture;
+    }
+
+    private RpcRequest createRequest(String className, String methodName, Object[] args) {
+        RpcRequest request = new RpcRequest();
+        request.setRequestId(UUID.randomUUID().toString());
+        request.setClassName(className);
+        request.setMethodName(methodName);
+        request.setParameters(args);
+
+        Class[] parameterTypes = new Class[args.length];
+        // Get the right class type
+        for (int i = 0; i < args.length; i++) {
+            parameterTypes[i] = getClassType(args[i]);
+        }
+        request.setParameterTypes(parameterTypes);
+//        Method[] methods = clazz.getDeclaredMethods();
+//        for (int i = 0; i < methods.length; ++i) {
+//            // Bug: if there are 2 methods have the same name
+//            if (methods[i].getName().equals(methodName)) {
+//                parameterTypes = methods[i].getParameterTypes();
+//                request.setParameterTypes(parameterTypes); // get parameter types
+//                break;
+//            }
+//        }
+
+        LOGGER.debug(className);
+        LOGGER.debug(methodName);
+        for (Class parameterType : parameterTypes) {
+            LOGGER.debug(parameterType.getName());
+        }
+        for (Object arg : args) {
+            LOGGER.debug(arg.toString());
+        }
+
+        return request;
+    }
+
+    private Class<?> getClassType(Object obj) {
+        Class<?> classType = obj.getClass();
+        String typeName = classType.getName();
+        switch (typeName) {
+            case "java.lang.Integer":
+                return Integer.TYPE;
+            case "java.lang.Long":
+                return Long.TYPE;
+            case "java.lang.Float":
+                return Float.TYPE;
+            case "java.lang.Double":
+                return Double.TYPE;
+            case "java.lang.Character":
+                return Character.TYPE;
+            case "java.lang.Boolean":
+                return Boolean.TYPE;
+            case "java.lang.Short":
+                return Short.TYPE;
+            case "java.lang.Byte":
+                return Byte.TYPE;
+        }
+
+        return classType;
+    }
+
+}
